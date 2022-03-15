b41a1bbc4061a36a996a38b5784e78ec644c2ae6
Juliet <agutu@gmail.com>
2022-03-15 15:02:18 +0300

    add icons onn search comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

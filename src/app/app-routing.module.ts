e736694a93da18d5dd48b52befcaff5493ac40ff
Juliet <agutu@gmail.com>
2022-03-16 22:54:16 +0300

    Route pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component'
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'search', component: SearchComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

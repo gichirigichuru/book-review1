596b9b1a168dcead69e9a512f6b53427e53eb61d
Juliet <agutu@gmail.com>
2022-03-17 14:26:23 +0300

    syle the search page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'search', component: SearchComponent }
+  
 
 ];
 
